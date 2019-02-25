63afc36339b8e0b895ecf974ebc1269f9dd74bbc
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-25 12:22:42 -0800

    implements queue

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,20 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # Empty Binary Search Tree
+        if self.value is None:
+            self.value = value
+        if self.value <= value:
+            self.right.insert(value)
+        if self.value > value:
+            self.left.insert(value)
+            
+    def contains(self, target):
+        pass
 
-  def contains(self, target):
-    pass
-
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,21 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        # ANSWER: we should use a list
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.size += 1
+        return self.storage.append(item)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.pop(0)
+
+    def len(self):
+        return self.size
