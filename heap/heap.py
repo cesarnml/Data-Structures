d543aa94f0d2760b1c5b7f7a6f5844219ba417b2
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-26 15:52:34 -0800

    fixing bubbleup

--- heap/heap.py
+++ heap/heap.py
@@ -2,19 +2,31 @@ class Heap:
     def __init__(self):
         self.storage = []
 
+    # Add value to the end of array.
+    # Then use _bubble_up to move value to correct index
     def insert(self, value):
         self.storage.append(value)
         self._bubble_up(len(self.storage) - 1)
 
+    # Store max_value
+    # Copy last element into first index
+    # Delete last element
+    # If the array isn't empty; call _sift_down to find
+    # correct position for new top value
+    # Return deleted value
     def delete(self):
         deleted = self.storage[0]
-        del(self.storage[0])
-        self._sift_down(0)
+        self.storage[0] = self.storage[len(self.storage) - 1]
+        del self.storage[len(self.storage) - 1]
+        if len(self.storage):
+            self._sift_down(0)
         return deleted
 
+    # max_value always at index 0
     def get_max(self):
         return self.storage[0]
 
+    # use built-in len function to get size of array
     def get_size(self):
         return len(self.storage)
 
