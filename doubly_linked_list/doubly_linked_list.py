a312e645acd37ebed418af75acd2e050f06aef32
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-26 15:11:49 -0800

    passing 4 heap test

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -48,13 +48,12 @@ class DoublyLinkedList:
         self.tail = node
 
     def add_to_head(self, value):
-    """
-    Two cases:
-    1. There is a head node. Call insert_before on current head.
-        Set new head to point to self.head.prev
-    2. There is no head. List is empty. Set head and tail to new ListNode.
-    """
-       if self.head:
+
+        # Two cases:
+        # 1. There is a head node. Call insert_before on current head.
+        #    Set new head to point to self.head.prev
+        # 2. There is no head. List is empty. Set head and tail to new ListNode.
+        if self.head:
             self.head.insert_before(value)
             self.head = self.head.prev
         else:
@@ -62,13 +61,12 @@ class DoublyLinkedList:
             self.tail = self.head
 
     def remove_from_head(self):
-    """
-    Two cases:
-    1. There is a head. new_head = self.head.next. Save current head value in temp.
-        .delete the current head. If the list is now empty, set tail to None.
-    2. There is no head. Nothing to do. Return None to indicate no work done.
-    """
-       if self.head:
+
+        # Two cases:
+        # 1. There is a head. new_head = self.head.next. Save current head value in temp.
+        #    .delete the current head. If the list is now empty, set tail to None.
+        # 2. There is no head. Nothing to do. Return None to indicate no work done.
+        if self.head:
             new_head = self.head.next
             temp = self.head.value
             self.head.delete()
--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,39 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        deleted = self.storage[0]
+        del(self.storage[0])
+        self._sift_down(0)
+        return deleted
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        while (index - 1) // 2 >= 0:
+            if self.storage[(index - 1) // 2] < self.storage[index]:
+                self.storage[(
+                    index - 1) // 2], self.storage[index] = self.storage[index], self.storage[(index - 1) // 2]
+            index = (index - 1) // 2
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        while index * 2 + 1 <= len(self.storage) - 1:
+            if index * 2 + 2 > len(self.storage) - 1:
+                maximum = index * 2 + 1
+            elif self.storage[index * 2 + 1] > self.storage[index * 2 + 2]:
+                maximum = index * 2 + 1
+            else:
+                maximum = index * 2 + 2
+
+        if self.storage[maximum] > self.storage[index]:
+            self.storage[maximum], self.storage[index] = self.storage[index], self.storage[maximum]
+        index = maximum
